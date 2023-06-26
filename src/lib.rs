07e27727c33a237d969712f607d9d6ba4ac4da54
root <rahul.soshte4771@gmail.com>
2023-06-26 10:11:30 +0000

    added stellar_baselib

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,2 @@
-pub fn placeholder_fn(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = placeholder_fn(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+use stellar_baselib;
+pub use stellar_baselib::*;
