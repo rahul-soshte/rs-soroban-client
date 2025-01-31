b516dcb6a9df7d01a8632d041be810c72908478b
Vincent Amouret <vincent.amouret@gmail.com>
2025-01-31 15:42:53 +0100

    test for jsonrpc

--- src/jsonrpc.rs
+++ src/jsonrpc.rs
@@ -84,3 +84,89 @@ pub struct Error {
     #[allow(dead_code)]
     pub message: Option<String>,
 }
+
+#[cfg(test)]
+mod test {
+
+    use std::collections::HashMap;
+    use std::str::FromStr;
+
+    use reqwest::Url;
+    use serde::Deserialize;
+    use serde_json::json;
+    use wiremock::matchers;
+    use wiremock::matchers::headers;
+    use wiremock::matchers::method;
+    use wiremock::matchers::path;
+    use wiremock::Mock;
+    use wiremock::MockServer;
+    use wiremock::ResponseTemplate;
+
+    use crate::jsonrpc::Response;
+    use crate::{jsonrpc::JsonRpc, server::Options};
+
+    #[derive(Debug, Deserialize, PartialEq, Eq)]
+    struct Data {
+        number: u64,
+        string: String,
+        vec: Vec<u8>,
+    }
+
+    #[tokio::test]
+    async fn test() {
+        let request = json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "echo",
+            "params": {
+                "number": 3,
+                "string": "a string",
+                "vec": [1, 2, 3]
+        }
+
+        });
+        let response = json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "result": {
+                "number": 3,
+                "string": "a string",
+                "vec": [1, 2, 3]
+            }
+        });
+        let mock_server = MockServer::start().await;
+
+        let response = ResponseTemplate::new(200).set_body_json(response);
+        Mock::given(method("POST"))
+            .and(path("/"))
+            .and(headers("x-api-key", vec!["9864920430304"]))
+            .and(matchers::body_partial_json(request))
+            .respond_with(response)
+            .expect(1..)
+            .mount(&mock_server)
+            .await;
+
+        let server_url = Url::from_str(&mock_server.uri()).unwrap();
+        let mut headers: HashMap<String, String> = HashMap::new();
+        headers.insert("x-api-key".into(), "9864920430304".into());
+        let rpc = JsonRpc::new(server_url, 10, headers);
+
+        let params = json!({
+                "number": 3,
+                "string": "a string",
+                "vec": [1, 2, 3]
+        });
+
+        let response: Response<Data> = rpc.post("echo", params).await.unwrap();
+        assert_eq!(response.id, 1);
+        assert_eq!(response.jsonrpc, "2.0");
+        assert_eq!(
+            response.result,
+            Some(Data {
+                number: 3,
+                string: "a string".to_string(),
+                vec: vec![1, 2, 3]
+            })
+        );
+    }
+}
